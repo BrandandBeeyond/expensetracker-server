20cff61f2c2ff0c0c095631efc318536ade47c55
dhiraj <dhirajmymodak22@gmail.com>
2025-08-02 16:41:59 +0530

    some fixes in url

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/User.model");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require("../config/config");
 
 const registerNewUser = async (req, res) => {
   try {
--- dbconnection.js
+++ dbconnection.js
@@ -3,7 +3,10 @@ const { MONGO_URI } = require('./config/config');
 
 const connectTodb = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("connected to db");
   } catch (error) {
     console.error(error);
